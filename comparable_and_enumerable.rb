381034e17e82b21873880e2dad79c0b20bd1224c
Raphael Adams <raphael.adams@shopify.com>
2020-10-26 14:01:21 -0400

    Added spaceship operator to Steak

--- comparable_and_enumerable.rb
+++ comparable_and_enumerable.rb
@@ -2,11 +2,17 @@
 # Raphael Adams
 
 class Steak
+    GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
     attr_accessor :grade
 
-    def >(other)
-        grade_scores = {"Prime" => 3, "Choice" => 2, "Select" => 1}
-        grade_scores[grade] > grade_scores[other.grade]
+    def <=>(other)
+        if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
+            return -1
+        elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
+            return 0
+        else
+            return 1
+        end
     end
 end
 
@@ -15,6 +21,5 @@ first_steak.grade = "Prime"
 second_steak = Steak.new
 second_steak.grade = "Choice"
 
-if first_steak > second_steak
-    puts "I'll take #{ first_steak.inspect }."
-end
+puts first_steak <=> second_steak
+puts second_steak <=> first_steak
